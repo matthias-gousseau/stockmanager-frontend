2db55823d889a5e8aaf3ad6baee004c1e31f4a95
Matthias Gousseau <matthias.gousseau@hotmail.com>
2025-12-02 13:39:25 +0100

    update tests unitaires

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('Auth', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        provideHttpClient(),        // injecter HttpClient
+        provideHttpClientTesting(), // remplacer HttpClient par le mock pour tests
+        provideRouter([])           // injecter Router vide
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
